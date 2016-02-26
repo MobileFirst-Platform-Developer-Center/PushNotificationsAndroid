85ec48e6ef166d3993fe260b43d713cd9c94c072
Eric N Garcia <garciaericn@gmail.com>
2016-02-26 09:47:07 -0500

    Updated copyright header.

--- PushNotification/app/src/test/java/com/ibm/sample/pushnotification/ExampleUnitTest.java
+++ PushNotification/app/src/test/java/com/ibm/sample/pushnotification/ExampleUnitTest.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2015 IBM Corp.
+/*
+ * Copyright 2016 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package com.ibm.sample.pushnotification;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
